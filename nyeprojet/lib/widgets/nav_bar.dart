8b45e848389ea0567cdd9e472790a6cf37cb4b44
adibanoor <buttadiba92@gmail.com>
2026-03-03 21:24:56 -0800

    la barre de nav utilisable sur toutes les pages

---
+++ nyeprojet/lib/widgets/nav_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class nav_bar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const nav_bar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(0, 0, 0, 86), // bleu foncé
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildNavItem(Icons.notifications_outlined, 0),
+          _buildNavItem(Icons.home, 1),
+          _buildNavItem(Icons.error_outline, 2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, int index) {
+    return IconButton(
+      onPressed: () => onItemTapped(index),
+      icon: Icon(
+        icon,
+        size: 28,
+        color: selectedIndex == index
+            ? Colors.blueAccent
+            : Colors.white,
+      ),
+    );
+  }
+}
